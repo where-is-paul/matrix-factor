9a0d1cbaeb04e899ebc42a417602ed98b1f640f8
Paul Liu <paul.liu.ubc@gmail.com>
2012-06-21 10:15:20 -0700

    some fixes to code. factorization is much faster now

--- csc_matrix_ildl_accumulated_nnzs_ver.h
+++ csc_matrix_ildl_accumulated_nnzs_ver.h
@@ -7,15 +7,14 @@
 #include <deque>
 
 template <class idx_type, class el_type>
-inline double norm(typename std::vector<el_type>& v, typename std::vector<idx_type>& curr_nnzs) { 
-    //optimize later. can use auto. can assign begin and end once so it is not evaluated every time. e.g.
-	//for (auto itr = new_values.begin(), end_itr = new_values.end(); itr != end_itr; ++itr )
-    typename std::vector<idx_type>::iterator it;
+inline double norm(typename std::vector<el_type>::iterator v_start, typename std::vector<el_type>::iterator v_end) { 
+    //optimize later
+    typename std::vector<el_type>::iterator it;
     double res = 0;
     //may want to use abs later if want to deal with complex
 	//#pragma omp parallel for reduction(+:res)
-    for (it = curr_nnzs.begin(); it != curr_nnzs.end(); it++) {
-        res += pow(std::abs(v[*it]), 2);  
+    for (it = v_start; it != v_end; it++) {
+        res += pow(std::abs(*it), 2);  
     }
     
     return res;
@@ -32,12 +31,12 @@ struct by_value {
 };
 
 template <class idx_type, class el_type>
-inline void drop_tol(std::vector<el_type>& v, std::vector<idx_type>& curr_nnzs, const int& lfil, const double& tol) { 
-    //optimize later. e.g. can use auto instead of manually declaring iterator type. see comments in norm func.
-    typename std::vector<idx_type>::iterator it;
-    el_type tolerance = tol*norm<idx_type, el_type>(v, curr_nnzs);
-    for (it = curr_nnzs.begin(); it != curr_nnzs.end(); it++) 
-        if (std::abs(v[*it]) < tolerance) v[*it] = 0;
+inline void drop_tol(std::vector<el_type>& v, const idx_type& k, std::vector<idx_type>& curr_nnzs, const int& lfil, const double& tol) { 
+    //optimize later
+    typename std::vector<el_type>::iterator it;
+    el_type tolerance = tol*norm<idx_type, el_type>(v.begin()+k, v.end());
+    for (it = v.begin()+k; it != v.end(); it++) 
+        if (std::abs(*it) < tolerance) *it = 0;
         
     //do lfil later. approach: use merge while doing delayed updates to obtain list of non zeros. sort curr_nnzs by value. only assign lfil largest to new vector
     by_value<idx_type, el_type> sorter(v);
@@ -47,7 +46,7 @@ inline void drop_tol(std::vector<el_type>& v, std::vector<idx_type>& curr_nnzs, 
 }
 
 template <class InputContainer, class InputIterator>
-inline void inplace_union(InputContainer& a, InputIterator const& b_start, InputIterator const& b_end)
+inline void inplace_union(InputContainer& a, InputIterator b_start, InputIterator b_end)
 {
     int mid = a.size(); //Store the end of first sorted range
 
@@ -67,22 +66,21 @@ void csc_matrix<idx_type, el_type> :: ildl(csc_matrix<idx_type, el_type>& L, elt
 {	
 	std::vector< std::deque< idx_type > > Llist;
 	std::vector<el_type> work(n_cols(), 0), Lfirst(n_cols(), 0);
-	std::vector<idx_type> curr_nnzs, all_nnzs;
+	std::vector<idx_type> curr_nnzs;
+	std::deque<idx_type> all_nnzs;
 	
 	int count = 0; 
 	idx_type i, j, k, offset;
 	el_type l_ik;
-	typename std::deque<idx_type>::const_iterator it; //can use auto
+	typename std::deque<idx_type>::const_iterator it;
 	
 	curr_nnzs.reserve(n_cols());
-
 	
 	Llist.resize(n_cols()); //reserve nspots for Llist.
 	
 	L.resize(n_rows(), n_cols(), (lfil+1)*n_cols()); //(+1 because there are 1s on the diagonal. they wont need to be stored if we want to optimize)
 	D.resize(n_cols()); //make a tridiagonal matrix class later? will need for 2x2 pivots.
 	
-	//the loop below can be made faster by assuming that the matrix has sorted row indices (so that we may iterate over m_col_idx instead).
 	//#pragma omp parallel for
 	for (i = 0; i < n_cols(); i++) {
 		D[i] = coeff(i,i);
@@ -92,6 +90,7 @@ void csc_matrix<idx_type, el_type> :: ildl(csc_matrix<idx_type, el_type>& L, elt
 	    //zero out work vector (opt. later)
 	    std::fill (work.begin() + k, work.end(), 0); //can change to work.begin() + k, but need to fix the norm after.
 	    
+		
 	    //the +1 is to avoid assigning diagonal element as nonzero since its stored in D already
 	    curr_nnzs.assign (m_row_idx.begin() + min(m_col_idx[k] + 1, m_col_idx[k+1]), m_row_idx.begin() + m_col_idx[k+1]); //the min is in case col_idx[k] == col_idx[k+1]
 		
@@ -102,56 +101,61 @@ void csc_matrix<idx_type, el_type> :: ildl(csc_matrix<idx_type, el_type>& L, elt
 			work[m_row_idx[j]] = m_x[j];
 		}
 		
-		if (k < n_cols() - 1)
-		for (it = Llist[k].begin(); it != Llist[k].end(); it++) { //Llist[k] contains the row idx of row k.
-			
-			offset = L.m_col_idx[*it] + Lfirst[*it] + 1; //find where A(k, k+1:n) starts
-			if (L.m_row_idx[offset] == k) {
-				Lfirst[*it]++; //update Lfirst
-			
-				//assumes matrix is symmetric with only lower triangular part stored. may want to change later.
-				for (j = offset+1; j < L.m_col_idx[*it+1]; j++) {
-					if (L.m_row_idx[j] > k)
-						work[L.m_row_idx[j]] -= L.m_x[offset] * D[*it] * L.m_x[j];
-				}
-				
-				
-				//find exactly where to merge with binary search. this function will probably be customized to deal with the linked lists
-				typename std::vector<idx_type>::iterator start = upper_bound(L.m_row_idx.begin() + L.m_col_idx[*it] + 1, L.m_row_idx.begin() + L.m_col_idx[*it+1], k);
-				inplace_union(curr_nnzs, start, L.m_row_idx.begin() + L.m_col_idx[*it+1]);
-				
-			}
-			
-		}		
-		
-		drop_tol(work, curr_nnzs, lfil, tol);	
-		
 		//get 1s on the diagonal
 		L.m_row_idx[count] = k;
 		L.m_x[count] = 1;
 		count++;
 		
-		
-		if (k < n_cols() - 1)
-		for (i = 0; i < (idx_type) std::min(lfil, (int) curr_nnzs.size()); i++) {
-		    L.m_row_idx[count] = curr_nnzs[i];
-		    L.m_x[count] = work[curr_nnzs[i]]/D[k];
-			
-			Llist[curr_nnzs[i]].push_back(k); //update Llist
-			
-			count++;
-		}
-		
-		L.m_col_idx[k+1] = count;
-		
 		if (k < n_cols() - 1) {
+			for (it = Llist[k].begin(); it != Llist[k].end(); it++) {
+				//update Lfirst
+				offset = L.m_col_idx[*it] + Lfirst[*it] + 1;
+				
+				if (L.m_row_idx[offset] == k) {
+					Lfirst[*it]++; //update Lfirst
+				
+					//assumes matrix is symmetric with only lower triangular part stored. may want to change later.
+					for (j = offset+1; j < L.m_col_idx[*it+1]; j++) {
+						if (L.m_row_idx[j] > k)
+							work[L.m_row_idx[j]] -= L.m_x[offset] * D[*it] * L.m_x[j];
+					}
+					
+				}
+				
+			}	
+			
+			//merge non-zero pattern of all accumulated previous cols with curr col. binary search to find starting point
+			inplace_union(curr_nnzs, upper_bound(all_nnzs.begin(), all_nnzs.end(), k), all_nnzs.end() );
+			
+			//<--- drop the elements that are <= k in index! (implement linked list later), when optimizing, no need to erase and reallocate every iter.
+			curr_nnzs.erase( remove_if(curr_nnzs.begin(), curr_nnzs.end(), [&k] (idx_type i) -> bool {return i <= k;}), curr_nnzs.end() );	
+			
+			drop_tol(work, k, curr_nnzs, lfil, tol);	
+
+			for (i = 0; i < (idx_type) std::min(lfil, (int) curr_nnzs.size()); i++) {
+				L.m_row_idx[count] = curr_nnzs[i];
+				L.m_x[count] = work[curr_nnzs[i]]/D[k];
+				
+				Llist[curr_nnzs[i]].push_back(k); //update Llist
+				
+				count++;
+			}
+		
+			//offset does not need to be calculated. its really just count (before count was changed by the for loop above.)
 			offset = L.m_col_idx[k] + Lfirst[k] + 1;
 			
-			for (i = offset; i < L.m_col_idx[k+1]; i++) {
+			for (i = offset; i < L.m_col_idx[k+1]; i++) {	//use Lfirst here later.
 				l_ik = L.m_x[i];
 				D[L.m_row_idx[i]] -= l_ik * D[k] * l_ik;	
 			}
 		}
+		
+		L.m_col_idx[k+1] = count;
+		
+		//make more explicit to the fact that it is accumulating unions. perhaps shorten all_nnzs as we go by removing elements <= k. have to change to deque then.
+		inplace_union(all_nnzs, L.m_row_idx.begin() + L.m_col_idx[k] + 1, L.m_row_idx.begin() + L.m_col_idx[k+1]);
+		
+		if (all_nnzs[0] == k) all_nnzs.pop_front();
 	}
 	
 	L.m_row_idx.resize(count);
--- ldl_driver.cpp
+++ ldl_driver.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cassert>
 #include <string.h>
+#include <sys/time.h>
 #include "csc_matrix.h"
 
 using namespace std;
@@ -18,7 +19,16 @@ int main(int argc, char* argv[]) {
 	assert( A.load(argv[3]) );
 	//cout << A << endl;
 	
+	struct timeval tim;  
+    gettimeofday(&tim, NULL);  
+    double t1=tim.tv_sec+(tim.tv_usec/1e6);  
+    
 	A.ildl(L, D, atof(argv[1]), atof(argv[2]));
+	
+	gettimeofday(&tim, NULL);  
+    double t2=tim.tv_sec+(tim.tv_usec/1e6);  
+    printf("The factorization took %.6lf seconds.\n", t2-t1);
+	printf("L has %d non-zeros.\n", L.nnz()); 
 	
 	if (argc > 4 && strcmp(argv[4], "-y") == 0) {
 	    cout << L << endl;
