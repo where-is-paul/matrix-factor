0772a883cd534d2f2aa8a7572c356b0ed1e55c56
Paul Liu <paul.liu.ubc@gmail.com>
2012-06-23 18:53:03 -0700

    added some documentation

--- CppSparse/python/tfem.py
+++
@@ -1,4 +0,0 @@
-import cppsparse as cp
-x = cp.dcsc(cp.dtrp("fem.dat"))
-x.sum_duplicates()
-print(x)
--- CppSparse/python/tvcat.py
+++
@@ -1,7 +0,0 @@
-import cppsparse as c
-x = c.dtrp("data.txt")
-print(x)
-y = c.dcsc(x)
-z = c.vcat(y, y)
-print(z)
-print(z.find())
