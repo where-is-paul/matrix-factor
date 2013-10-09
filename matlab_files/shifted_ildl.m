66e6f38fd7ba01a13f6109c4334a5f32d8105025
Paul Liu <Paul@deep-thought.(none)>
2013-10-09 00:19:27 -0700

    shifted ildl matlab function updated

--- matlab_files/shifted_ildl.m
+++ matlab_files/shifted_ildl.m
@@ -1,8 +1,8 @@
 function [L, D, p, S, B] = ildl(A, opts)
 % version of ildl that applies a shift matrix to the specified
 % problem. to be more precise, the block diagonal matrix  with
-% diagonal block alpha * [0 1; 1 0] is added to the original matrix,
-% producing a positive shift in its eigenvalues.
+% diagonal block alpha * ||A||_1 * [0 1; 1 0] is added to the 
+% original matrix, producing a positive shift in its eigenvalues.
     
     if (nargin < 2)
         opts.type = 'default';
@@ -15,24 +15,45 @@ function [L, D, p, S, B] = ildl(A, opts)
         opts.tol = 0.001;
     end
     if (~any(strcmp('pp_tol',fieldnames(opts))))
-        opts.pp_tol = 1.00;
+        opts.pp_tol = 2.00;
     end
     if (~any(strcmp('ordering',fieldnames(opts))))
         opts.ordering = 'amd';
     end
     if (~any(strcmp('shift_factor',fieldnames(opts))))
-        opts.shift_factor = 0.001;
+        opts.shift_factor = 0.000;
+    end
+    if (~any(strcmp('equil',fieldnames(opts))))
+        opts.equil = 'y';
     end
     
     display(opts);
     
+    if (~issparse(A))
+        error('Input matrix must be sparse');
+    end
+    if (norm(A-A',1) ~= 0)
+        error('Input matrix must be symmetric');
+    end
+    % sort of wasteful, but we'll perform a first factorization
+    % to get A equilibriated and reorded
+    % and then a second factorization on the B, with equilibriation
+    % and permutation turned off
+    [~, ~, p, S, C] = ildl(A, opts.fill_factor, opts.tol, ...
+                           opts.pp_tol, opts.ordering, opts.equil);  
+
+    % turn off equilibriation and ordering
+    opts.equil = 'n';
+    opts.ordering = 'none';
+    
+    % perform a shift on our matrix
     n = size(A,1);
     d = mod(1:n,2);
     shift = spdiags(d', -1, n, n) + spdiags(d', -1, n, n)';
-    A = opts.shift_factor*shift;
-    [L D p S B] = ildl(A, opts.fill_factor, ...
-                       opts.tol, opts.pp_tol, opts.ordering);
-    
+    C = C+opts.shift_factor*shift;
+
+    [L, D, ~, ~, B] = ildl(C, opts.fill_factor, opts.tol, ...
+                       opts.pp_tol, opts.ordering, opts.equil);  
     
 end
 
