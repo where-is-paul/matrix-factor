8407af3a5a3b03af1840d74e9da0c7f9e3239b37
Paul Liu <paul.liu.ubc@gmail.com>
2016-02-18 05:36:59 -0800

    Another round of documentation updates.

--- html/search/classes_0.js
+++ html/search/classes_0.js
@@ -1,7 +1,4 @@
 var searchData=
 [
-  ['block_5fdiag_5fmatrix',['block_diag_matrix',['../classblock__diag__matrix.html',1,'']]],
-  ['by_5fdegree',['by_degree',['../structby__degree.html',1,'']]],
-  ['by_5ftolerance',['by_tolerance',['../structby__tolerance.html',1,'']]],
-  ['by_5fvalue',['by_value',['../structby__value.html',1,'']]]
+  ['block_5fdiag_5fmatrix',['block_diag_matrix',['../classblock__diag__matrix.html',1,'']]]
 ];
